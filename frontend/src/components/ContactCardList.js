d0c8acc06aad9374d09f53e99b60ee33f0327026
The Boilerplate Life <theboilerplate@gmail.com>
2020-08-14 23:00:02 -0700

    Some UI tweaks

--- frontend/src/components/ContactCardList.js
+++ frontend/src/components/ContactCardList.js
@@ -1,29 +1,27 @@
 import { CircularProgress } from '@material-ui/core';
 import React from 'react';
 import CardColumns from 'react-bootstrap/CardColumns';
-import Container from 'react-bootstrap/Container';
+import '../styles/contact-card-list.scss';
 import ContactCard from './ContactCard';
 
 const ContactCardList = ({ searchText, contacts, isListing, saveFunc }) => {
 
   return (
-    <>
-      <Container className="d-flex justify-content-center">
-        {
-          isListing ?
-            <CircularProgress /> :
-            contacts.length > 0 ?
-              <CardColumns>
-                {
-                  contacts.map(contact => <ContactCard key={contact._id} contact={contact} saveFunc={saveFunc} />)
-                }
-              </CardColumns> :
-              searchText ?
-                <p className="text-center text-info">No contacts found for the given search text</p> :
-                <p className="text-center text-info">Use the search to display some contacts</p>
-        }
-      </Container>
-    </>
+    <div className="d-flex justify-content-center">
+      {
+        isListing ?
+          <CircularProgress /> :
+          contacts.length > 0 ?
+            <CardColumns>
+              {
+                contacts.map(contact => <ContactCard key={contact._id} contact={contact} saveFunc={saveFunc} />)
+              }
+            </CardColumns> :
+            searchText ?
+              <p className="text-center text-info">No contacts found for the given search text</p> :
+              <p className="text-center text-info">Use the search to display some contacts</p>
+      }
+    </div>
   );
 }
 
--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -4,31 +4,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 const NavigationBar = () => (
-  <>
-    <Navbar bg="dark" variant="dark" sticky="top" expand="md">
-      <Navbar.Brand href="/">Online Directory App</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="w-100">
-          <Nav.Item>
-            <IndexLinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-            </IndexLinkContainer>
-          </Nav.Item>
-          <Nav.Item className="w-100">
-            <LinkContainer to="/admin">
-              <Nav.Link>Admin</Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-          <Nav.Item style={{ minWidth: '16rem' }}>
-            <LinkContainer to="/profile">
-              <Nav.Link><span>Signed in as: </span><u>The Boilerplate Life</u></Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </>
+  <Navbar bg="dark" variant="dark" sticky="top" expand="md">
+    <Navbar.Brand href="/">Online Directory App</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="w-100">
+        <Nav.Item>
+          <IndexLinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </IndexLinkContainer>
+        </Nav.Item>
+        <Nav.Item className="w-100">
+          <LinkContainer to="/admin">
+            <Nav.Link>Admin</Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+        <Nav.Item style={{ minWidth: '16rem' }}>
+          <LinkContainer to="/profile">
+            <Nav.Link><span>Signed in as: </span><u>The Boilerplate Life</u></Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default NavigationBar;
