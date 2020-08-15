c42863553362f26be5a311ef3cc4e7b56b9878df
The Boilerplate Life <theboilerplate@gmail.com>
2020-08-15 03:47:06 -0700

    Implementing add contact functionality in the Admin page

--- frontend/src/redux/reducers/contacts.js
+++ frontend/src/redux/reducers/contacts.js
@@ -1,20 +1,22 @@
 import {
-  GET_CONTACT, GET_CONTACT_ERROR, GET_CONTACT_SUCCESS,
+  ADD_CONTACT, ADD_CONTACT_ERROR, ADD_CONTACT_SUCCESS, GET_CONTACT, GET_CONTACT_ERROR, GET_CONTACT_SUCCESS,
   LIST_CONTACTS, LIST_CONTACTS_ERROR, LIST_CONTACTS_SUCCESS,
-  SAVE_CONTACT, SAVE_CONTACT_ERROR, SAVE_CONTACT_SUCCESS
+  UPDATE_CONTACT, UPDATE_CONTACT_ERROR, UPDATE_CONTACT_SUCCESS
 } from '../actionTypes';
 
 const contacts = (
   state = {
     isListing: false,
-    isSaving: false,
+    isUpdating: false,
     isGetting: false,
+    isAdding: false,
     searchText: '',
     contacts: [],
     profileContact: null,
     listError: null,
     saveError: null,
-    getError: null
+    getError: null,
+    addError: null
   },
   action) => {
   switch (action.type) {
@@ -24,18 +26,24 @@ const contacts = (
       return Object.assign({}, state, { isListing: false, searchText: action.searchText, contacts: action.contacts, listError: null });
     case LIST_CONTACTS_ERROR:
       return Object.assign({}, state, { isListing: false, listError: action.error });
-    case SAVE_CONTACT:
-      return Object.assign({}, state, { isSaving: true, saveError: null });
-    case SAVE_CONTACT_SUCCESS:
-      return Object.assign({}, state, { isSaving: false, profileContact: action.contact, saveError: null });
-    case SAVE_CONTACT_ERROR:
-      return Object.assign({}, state, { isSaving: false, saveError: action.error });
+    case UPDATE_CONTACT:
+      return Object.assign({}, state, { isUpdating: true, saveError: null });
+    case UPDATE_CONTACT_SUCCESS:
+      return Object.assign({}, state, { isUpdating: false, profileContact: action.contact, saveError: null });
+    case UPDATE_CONTACT_ERROR:
+      return Object.assign({}, state, { isUpdating: false, saveError: action.error });
     case GET_CONTACT:
       return Object.assign({}, state, { isGetting: true, getError: null });
     case GET_CONTACT_SUCCESS:
       return Object.assign({}, state, { isGetting: false, profileContact: action.contact, getError: null });
     case GET_CONTACT_ERROR:
       return Object.assign({}, state, { isGetting: false, getError: action.error });
+    case ADD_CONTACT:
+      return Object.assign({}, state, { isAdding: true, addError: null });
+    case ADD_CONTACT_SUCCESS:
+      return Object.assign({}, state, { isAdding: false, addError: null });
+    case ADD_CONTACT_ERROR:
+      return Object.assign({}, state, { isAdding: false, addError: action.error });
     default:
       return state;
   }
