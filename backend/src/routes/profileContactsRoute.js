e808cfb99bb458c0709df3d13a8aba3bd487c1a0
The Boilerplate Life <theboilerplatelife@gmail.com>
2020-08-26 08:38:52 -0700

    Replacing callbacks with promises

--- backend/src/routes/profileContactsRoute.js
+++ backend/src/routes/profileContactsRoute.js
@@ -6,45 +6,39 @@ const authz = require('../utils/authz');
 
 // Create profile contact
 router.post('/', (req, res, next) => {
-  delete req.body.idpSub;
   authz.enforceAuthorization(
     req.user,
     ['create:profile_contact'],
     req, res, next,
     (req, res, next) => {
       const idpSub = req.user.sub;
-      repository.getContactByIdpSubject(
-        idpSub,
-        (err, existingProfileContact) => {
-          if (err) return next(err);
+      repository.getContactByIdpSubject(idpSub)
+        .catch(err => {
+          if (err.statusCode !== 404) {
+            throw err;
+          }
+        })
+        .then(existingProfileContact => {
           if (existingProfileContact) {
             const conflictError = new Error(`Profile contact already exists for IDP sub ${idpSub}`);
             conflictError.statusCode = 409;
-            return next(conflictError);
+            throw conflictError;
           }
-          auth0.getAccessToken()
-            .then(accessToken => auth0.getUser(accessToken, idpSub))
-            .then(user => repository.updateContact(
-              user.user_metadata.contact_id,
-              { idpSubject: idpSub },
-              (err, profileContact) => {
-                if (err) return next(err);
-                if (!profileContact) {
-                  return next(new Error(`Something went wrong. Could not create profile contact for ${idpSub}.`));
-                }
-                authz.isAdmin(
-                  req.user,
-                  (err, isAdmin) => {
-                    if (err) { return next(err); }
-                    res.json(Object.assign(profileContact, { admin: isAdmin }));
-                  });
-              },
-              true))
-            .catch(err => handleError(err, next));
-        },
-        true
-      );
-    });
+        })
+        .then(() => auth0.getAccessToken()
+          .then(accessToken => auth0.getUser(accessToken, idpSub)
+            .then(user => Promise.all([
+              user.user_metadata?.contact_id ?
+                repository.updateContact(user.user_metadata.contact_id, { idpSubject: idpSub }, true) :
+                repository.addContact({ name: user.name, idpSubject: idpSub }),
+              authz.isAdmin(accessToken, req.user)
+            ]))
+            .then(([profileContact, isAdmin]) => {
+              res.json(Object.assign(profileContact, { admin: isAdmin }));
+            })))
+        .catch(err => handleError(err, next));
+    }
+  );
 });
 
 // Get profile contact
@@ -53,24 +47,14 @@ router.get('/', (req, res, next) => {
     req.user,
     ['read:profile_contact'],
     req, res, next,
-    (req, res, next) => repository.getContactByIdpSubject(
-      req.user.sub,
-      (err, profileContact) => {
-        if (err) return next(err);
-        if (!profileContact) {
-          const notFoundError = new Error(`There is no profile contact for IDP sub ${req.user.sub}. Create one first.`);
-          notFoundError.statusCode = 404;
-          return next(notFoundError);
-        }
-        authz.isAdmin(
-          req.user,
-          (err, isAdmin) => {
-            if (err) { return next(err); }
-            res.json(Object.assign(profileContact, { admin: isAdmin }));
-          });
-      },
-      true
-    ));
+    (req, res, next) => repository.getContactByIdpSubject(req.user.sub, true)
+      .then(profileContact => auth0.getAccessToken()
+        .then(accessToken => authz.isAdmin(accessToken, req.user))
+        .then(isAdmin => {
+          res.json(Object.assign(profileContact, { admin: isAdmin }));
+        }))
+      .catch(err => handleError(err, next))
+  );
 });
 
 // Update profile contact
@@ -80,29 +64,13 @@ router.patch('/', (req, res, next) => {
     req.user,
     ['update:profile_contact'],
     req, res, next,
-    (req, res, next) => repository.getContactByIdpSubject(
-      req.user.sub,
-      (err, existingProfileContact) => {
-        if (err) return next(err);
-        if (!existingProfileContact) {
-          const notFoundError = new Error(`There is no profile contact for IDP sub ${req.user.sub}. Create one first.`);
-          notFoundError.statusCode = 404;
-          return next(notFoundError);
-        }
-        repository.updateContact(
-          existingProfileContact._id,
-          req.body,
-          (err, profileContact) => {
-            if (err) return next(err);
-            if (!profileContact) {
-              return next(new Error(`Something went wrong. Could not update profile contact for ${idpSub}.`));
-            }
-            res.json(profileContact);
-          }
-        );
-      },
-      true
-    ));
+    (req, res, next) => repository.getContactByIdpSubject(req.user.sub,)
+      .then(existingProfileContact => repository.updateContact(existingProfileContact._id, req.body))
+      .then(updatedProfileContact => {
+        res.json(updatedProfileContact);
+      })
+      .catch(err => handleError(err, next))
+  );
 });
 
 const handleError = (err, next) => {
