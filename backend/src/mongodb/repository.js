24577ff8ec64fc68dfdf07a74c88df18fd9d495c
theboilerplatelife <theboilerplatelife@gmail.com>
2025-07-12 10:11:14 -0500

    Fix mongo buffering timeout - wait on mongo connection to be established

--- backend/src/mongodb/repository.js
+++ backend/src/mongodb/repository.js
@@ -1,65 +1,69 @@
 /** @format */
 
 const mongoose = require('mongoose');
-mongoose
+const connectionPromise = mongoose
   .connect(process.env.API_MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) =>
     console.log(
       `Established connection to MongoDB on host ${result.connection.host}:${result.connection.port} with user ${result.connection.user}`
     )
   )
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error(err);
+    throw err;
+  });
 const errors = require('../utils/errors');
 
 const contactSchema = require('./schema/contactSchema');
 const Contact = mongoose.model('Contact', contactSchema.schema);
 
 // Add contact
-const addContact = (contact) => {
-  return new Contact(contact).save().then((savedContact) => {
-    if (!savedContact) {
-      throw new Error('Something went wrong. Could not add contact for the given input.');
-    }
-    return savedContact;
-  });
+const addContact = async (contact) => {
+  await connectionPromise;
+  const savedContact = await new Contact(contact).save();
+  if (!savedContact) {
+    throw new Error('Something went wrong. Could not add contact for the given input.');
+  }
+  return savedContact;
 };
 
 // Get contact by ID
-const getContactById = (contactId) => {
-  return Contact.findById(contactId).then((existingContact) => {
-    if (!existingContact) {
-      throw errors.generateError(`No contact found for ID ${contactId}`, 404);
-    }
-    return existingContact;
-  });
+const getContactById = async (contactId) => {
+  await connectionPromise;
+  const existingContact = await Contact.findById(contactId);
+  if (!existingContact) {
+    throw errors.generateError(`No contact found for ID ${contactId}`, 404);
+  }
+  return existingContact;
 };
 
 // Get contact by IDP subject
-const getContactByIdpSubject = (idpSub, lean = false) => {
-  return Contact.findOne({ idpSubjects: idpSub })
-    .lean(lean)
-    .then((contact) => {
-      if (!contact) {
-        throw errors.generateError(`There is no profile contact for IDP sub ${idpSub}. Create one first.`, 404);
-      }
-      return contact;
-    });
+const getContactByIdpSubject = async (idpSub, lean = false) => {
+  await connectionPromise;
+  const contact = await Contact.findOne({ idpSubjects: idpSub }).lean(lean);
+  if (!contact) {
+    throw errors.generateError(`There is no profile contact for IDP sub ${idpSub}. Create one first.`, 404);
+  }
+  return contact;
 };
 
 // Update contact by ID
-const updateContact = (contactId, contact, lean = false) => {
-  return Contact.findByIdAndUpdate(contactId, contact, { new: true, useFindAndModify: false, runValidators: true })
-    .lean(lean)
-    .then((updatedContact) => {
-      if (!updatedContact) {
-        throw new Error('Something went wrong. Could not update contact for the given input.');
-      }
-      return updatedContact;
-    });
-};
+const updateContact = async (contactId, contact, lean = false) => {
+  await connectionPromise;
+  const updatedContact = await Contact.findByIdAndUpdate(
+    contactId,
+    contact,
+    { new: true, useFindAndModify: false, runValidators: true }
+  ).lean(lean);
+  if (!updatedContact) {
+    throw new Error('Something went wrong. Could not update contact for the given input.');
+  }
+  return updatedContact;
+}
 
 // List contacts by keyword
-const listContactsByKeyword = (keyword) => {
+const listContactsByKeyword = async (keyword) => {
+  await connectionPromise;
   const escapedKeyword = escapeRegexCharacters(keyword);
   return Contact.find(
     escapedKeyword === ':all:'
